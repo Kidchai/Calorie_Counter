66560ded5e7ee53741bc8deb30c4e9f2a65d07b7
Kidchai <o.naibova@gmail.com>
2022-10-15 09:24:09 -0500

    Implement conversion from Meal to MealTo

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,22 @@
 package ru.javawebinar.topjava.web.meal;
 
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.List;
 
 public class MealRestController {
     private MealService service;
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public List<MealTo> getAll() {
+        int userId = SecurityUtil.authUserId();
+        return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
+    }
 
 }
