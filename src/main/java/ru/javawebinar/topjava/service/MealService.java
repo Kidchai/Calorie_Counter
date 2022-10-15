fa7dc88a86a3280ac1a8fac58dfca4719210c263
Kidchai <o.naibova@gmail.com>
2022-10-15 08:30:51 -0500

    Implement MealService

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,34 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.Collection;
 
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
+
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(int userId, Meal meal) {
+        return repository.save(userId, meal);
+    }
+
+    public boolean delete(int userId, int mealId) {
+        return repository.delete(userId, mealId);
+    }
+
+    public Meal get(int userId, int mealId) {
+        return repository.get(userId, mealId);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+
 
 }
