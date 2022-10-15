b774b10455b161584b71a78a3382365b54c80ef0
Kidchai <o.naibova@gmail.com>
2022-10-15 09:49:16 -0500

    Add create, update, delete, get methods to MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -8,15 +12,41 @@ import ru.javawebinar.topjava.web.SecurityUtil;
 import java.util.List;
 
 public class MealRestController {
-    private MealService service;
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+    private final MealService service;
 
     public MealRestController(MealService service) {
         this.service = service;
     }
 
-    public List<MealTo> getAll() {
+    public Meal create(Meal meal) {
         int userId = SecurityUtil.authUserId();
-        return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
+        log.info("create new meal {} for user {}", meal, userId);
+        return service.create(SecurityUtil.authUserId(), meal);
     }
 
+    public Meal update(Meal meal) {
+        int userId = SecurityUtil.authUserId();
+        log.info("update meal {} for user {}", meal, userId);
+        return service.update(userId, meal);
+    }
+
+    public void delete(Meal meal) {
+        int userId = SecurityUtil.authUserId();
+        log.info("delete meal {} for user {}", meal, userId);
+        service.delete(userId, meal.getId());
+    }
+
+    public Meal get(int mealId) {
+        int userId = SecurityUtil.authUserId();
+        log.info("delete meal {} for user {}", mealId, userId);
+        return service.get(userId, mealId);
+    }
+
+    public List<MealTo> getAll(User user) {
+        int userId = SecurityUtil.authUserId();
+        log.info("get all meals for user {}", userId);
+        return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
+    }
 }
